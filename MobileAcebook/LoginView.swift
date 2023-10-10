4aecd29e4028be11b71c5a0851668d62057fa7b2
Claudia_Alves <claudiascarvalves@gmail.com>
2023-10-10 12:23:20 +0100

    updated navigation link in signup page

---
+++ MobileAcebook/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  MobileAcebook
+//
+//  Created by Claudia on 10/10/2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Hello login")
+    }
+}
+
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}

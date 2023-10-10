06fa07b55425875a4e10fe0ab6fa743a6e803a37
Claudia_Alves <claudiascarvalves@gmail.com>
2023-10-10 14:16:28 +0100

    first changes to posts page

--- MobileAcebook/PostView.swift
+++ MobileAcebook/PostView.swift
@@ -8,10 +8,44 @@
 import SwiftUI
 
 struct PostView: View {
+    @State private var post = ""
     var body: some View {
-        Text("Hello")
+        ScrollView {
+            VStack {
+                HStack {
+                    Image("makers-logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 80, height: 80)
+                        .accessibilityIdentifier("makers-logo")
+                    Spacer() // Pushes the Log Out button to the right
+                    Button("Log Out") {
+                        // Add your log out action here
+                    }
+                    .accessibilityIdentifier("logOutButton")
+                }
+                .padding(.horizontal)
+                HStack{
+                    AsyncImage(url: /*@START_MENU_TOKEN@*/URL(string: "https://example.com/icon.png")/*@END_MENU_TOKEN@*/)
+                    
+                    TextField("Penny for your thoughts", text:$post)
+                    //                    .background(Color.blue)
+                        .foregroundColor(.black)
+                        .cornerRadius(10)
+                        .padding()
+                }
+                Text("Hello")
+                Spacer() // Pushes the Hello text to the top
+
+                // Add your scrollable content here
+                ForEach(1..<20) { i in
+                    Text("Item \(i)")
+                        .font(.largeTitle)
+                }
+            }
+        }
     }
-}
+        }
 
 
 struct PostView_Previews: PreviewProvider {

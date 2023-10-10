58f00a114f78e8e31da8d5670186a5d110e9d65d
Tom Whelan <thomas.whelan20@icloud.com>
2023-10-10 14:18:47 +0100

    Started login requirements

--- MobileAcebook/SignUpPageView.swift
+++ MobileAcebook/SignUpPageView.swift
@@ -19,7 +19,8 @@ struct SignUpPageView: View {
     @State private var username = ""
     @State private var password = ""
     @State private var confirmPass = ""
-//    @State private var attachFile = ""
+    @State private var errorMessage: String? = nil
+    //    @State private var attachFile = ""
     
     var body: some View {
         
@@ -42,9 +43,14 @@ struct SignUpPageView: View {
                     Section(header: Text("Sign Up")){
                         TextField("Email", text: $email)
                         TextField("Username", text: $username)
-                        TextField("Password", text: $password)
-                        TextField("Confirm Password", text: $confirmPass)
+                        SecureField("Password", text: $password)
+                        SecureField("Confirm Password", text: $confirmPass)
                     }
+                    if let error = errorMessage {
+                                Text(error)
+                                    .foregroundColor(.red)
+                    }
+                    
                 }
                 .frame(width: 400, height: 500)
                 .scrollContentBackground(.hidden)
@@ -56,31 +62,50 @@ struct SignUpPageView: View {
                     .foregroundColor(.white)
                     .font(.system(size: 20, weight: .bold))
                     .cornerRadius(25)
-                    .padding(.top, 50)
                 
             }
         }
     }
     
+    func isValidUserName() -> Bool {
+        return username.count >= 5
+    }
+    
+    func isValidPassword() -> Bool {
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
+        return passwordTest.evaluate(with: password)
+    }
+    
+    func isValidEmail() -> Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailPredicate.evaluate(with: email)
+    }
+    
     func submitUser() {
-        if password == confirmPass {
+        if !isValidUserName() {
+            errorMessage = "Invalid Username"
+            return
+        } else if !isValidPassword() {
+            errorMessage = "Passwords must contain a special character,8 letters, a number and capital letter"
+            return
+        } else if !isValidEmail(){
+            errorMessage = "Please enter a valid email"
+            return
+        } else if password == confirmPass {
             let user = User(email: email, username: username, password: password)
             authenticationService.signUp(user: user)
             email = ""
             username = ""
             password = ""
             confirmPass = ""
-        } else {
-            password = "Passwords do not match"
-            confirmPass = ""
+            errorMessage = nil
         }
-        
-        
     }
-}
-
-struct SignUpPageView_Previews: PreviewProvider {
-    static var previews: some View {
-        SignUpPageView(authenticationService: AuthenticationService())
+    
+    struct SignUpPageView_Previews: PreviewProvider {
+        static var previews: some View {
+            SignUpPageView(authenticationService: AuthenticationService())
+        }
     }
 }

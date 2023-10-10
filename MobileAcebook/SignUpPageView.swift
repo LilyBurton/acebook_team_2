bd0ffc162564c4d48ccc5213bbc76d48677b8694
Kumani K <kumanikidd@gmail.com>
2023-10-10 10:18:22 +0100

    Sign up update

---
+++ MobileAcebook/SignUpPageView.swift
@@ -0,0 +1,86 @@
+//
+//  SignUpPageView.swift
+//  MobileAcebook
+//
+//  Created by Kumani Kidd on 09/10/2023.
+//
+
+import SwiftUI
+
+struct SignUpPageView: View {
+    let authenticationService: AuthenticationService
+    
+    init(authenticationService: AuthenticationService) {
+        self.authenticationService = authenticationService
+    }
+    
+    
+    @State private var email = ""
+    @State private var username = ""
+    @State private var password = ""
+    @State private var confirmPass = ""
+//    @State private var attachFile = ""
+    
+    var body: some View {
+        
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [ .blue, Color("lightblue")]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+            .ignoresSafeArea()
+            
+            VStack {
+                Image("makers-logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    .accessibilityIdentifier("makers-logo")
+                
+                Spacer()
+                
+                Form {
+                    Section(header: Text("Sign Up")){
+                        TextField("Email", text: $email)
+                        TextField("Username", text: $username)
+                        TextField("Password", text: $password)
+                        TextField("Confirm Password", text: $confirmPass)
+                    }
+                }
+                .frame(width: 400, height: 500)
+                .scrollContentBackground(.hidden)
+                
+                Spacer()
+                Button ("Submit", action: submitUser)
+                    .frame(width: 280, height: 50)
+                    .background(.orange.gradient)
+                    .foregroundColor(.white)
+                    .font(.system(size: 20, weight: .bold))
+                    .cornerRadius(25)
+                    .padding(.top, 50)
+                
+            }
+        }
+    }
+    
+    func submitUser() {
+        if password == confirmPass {
+            let user = User(email: email, username: username, password: password)
+            authenticationService.signUp(user: user)
+            email = ""
+            username = ""
+            password = ""
+            confirmPass = ""
+        } else {
+            password = "Passwords do not match"
+            confirmPass = ""
+        }
+        
+        
+    }
+}
+
+struct SignUpPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpPageView(authenticationService: AuthenticationService())
+    }
+}
--- MobileAcebook/WelcomePageView.swift
+++ MobileAcebook/WelcomePageView.swift
@@ -2,38 +2,52 @@
 //  WelcomePageView.swift
 //  MobileAcebook
 //
-//  Created by Josué Estévez Fernández on 30/09/2023.
+//  Created by Kumani Kidd on 09/10/2023.
 //
 
 import SwiftUI
 
 struct WelcomePageView: View {
+    let authenticationService: AuthenticationService
+    
+    init(authenticationService: AuthenticationService) {
+        self.authenticationService = authenticationService
+    }
+    
     var body: some View {
-        ZStack {
-            VStack {
-                Spacer()
-
-                Text("Welcome to Acebook!")
-                    .font(.largeTitle)
-                    .padding(.bottom, 20)
-                    .accessibilityIdentifier("welcomeText")
-
-                Spacer()
-
-                Image("makers-logo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200, height: 200)
-                    .accessibilityIdentifier("makers-logo")
-                
-                Spacer()
-
-                Button("Sign Up") {
-                    // TODO: sign up logic
+        NavigationView {
+            ZStack {
+                VStack {
+                    Spacer()
+                    
+                    Text("Welcome to Acebook!")
+                        .font(.largeTitle)
+                        .padding(.bottom, 20)
+                        .accessibilityIdentifier("welcomeText")
+                    
+                    Spacer()
+                    
+                    Image("makers-logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 200)
+                        .accessibilityIdentifier("makers-logo")
+                    
+                    Spacer()
+                    
+                    NavigationLink(destination: SignUpPageView(authenticationService: authenticationService)) {
+                        Text("Sign up")
+                            .padding()
+                    }
+                    .accessibilityIdentifier("signUpButton")
+                    
+                    Button("Login") {
+                        
+                    }
+                    .accessibilityIdentifier("loginButton")
+                    
+                    Spacer()
                 }
-                .accessibilityIdentifier("signUpButton")
-                
-                Spacer()
             }
         }
     }
@@ -41,6 +55,6 @@ struct WelcomePageView: View {
 
 struct WelcomePageView_Previews: PreviewProvider {
     static var previews: some View {
-        WelcomePageView()
+        WelcomePageView(authenticationService: AuthenticationService())
     }
 }
